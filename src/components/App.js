2dcd5b9ade3ad12bf5431a2a3bcd3ff275d85bd2
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-04-16 01:56:27 +0530

    should work

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../redux/action/counterAction";
 
 const App = () => {
+  const val = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>{val}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
